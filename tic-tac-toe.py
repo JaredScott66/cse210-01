0407dcd6f40c28ad0e8e22da8f8f08a2367cb4ce
Jared Scott <jaredstscott@juno.com>
2022-09-16 23:35:31 -0500

    started function to update game

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,14 +1,23 @@
-print("Lets play tica-tac-toe!\nX starts!")
-while True:
-    first = "1"
-    second = "2"
-    third = "3"
-    fourth = "4"
-    fifth = "5"
-    sixth = "6"
-    seventh = "7"
-    eigth = "8"
-    ninth = "9"
-    grid_show = print(first + "|" + second + "|" + third), print(fourth + "|" + fifth + "|" + sixth), print(seventh + "|" + eigth + "|" + ninth)
-    x_choice = input("X's turn to choose a square (1-9): ")
+def main():
+
+    def board_update(player_input, player):
+        first = "1"
+        second = "2"
+        third = "3"
+        fourth = "4"
+        fifth = "5"
+        sixth = "6"
+        seventh = "7"
+        eigth = "8"
+        ninth = "9"
+        side = player
+        grid_show = print(first + "|" + second + "|" + third), print(fourth + "|" + fifth + "|" + sixth), print(seventh + "|" + eigth + "|" + ninth)
+
+
+
+
+    print("Lets play tica-tac-toe!\nX starts!")
+    while True:
+        
+        x_choice = input("X's turn to choose a square (1-9): ")
 

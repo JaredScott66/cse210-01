ccf58d9da0f366bb730e5a34d3f338b18b86e87f
Jared Scott <jaredstscott@juno.com>
2022-09-16 23:06:39 -0500

    Created Game grid

---
+++ tic-tac-toe.py
@@ -0,0 +1,14 @@
+print("Lets play tica-tac-toe!\nX starts!")
+while True:
+    first = "1"
+    second = "2"
+    third = "3"
+    fourth = "4"
+    fifth = "5"
+    sixth = "6"
+    seventh = "7"
+    eigth = "8"
+    ninth = "9"
+    grid_show = print(first + "|" + second + "|" + third), print(fourth + "|" + fifth + "|" + sixth), print(seventh + "|" + eigth + "|" + ninth)
+    x_choice = input("X's turn to choose a square (1-9): ")
+

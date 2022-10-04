51408f1d304f2117da0b09b831d8b195d1edfab5
Jared Scott <jaredstscott@juno.com>
2022-10-04 18:48:01 -0500

    added rfk

---
+++ code_test.py
@@ -0,0 +1,12 @@
+from class_test import Multiply
+from class_test import Exponant
+
+expo = Exponant()
+
+expo.do_math(2, 5)
+product = expo.get_product()
+expo.make_expo(2, 2)
+butter = expo.get_expo()
+
+print(butter)
+print(product)
---
+++ rfk-incomplete/rfk-incomplete/rfk/game/casting/artifact.py
@@ -0,0 +1,9 @@
+from game.casting.actor import Actor
+
+# TODO: Implement the Artifact class here. Don't forget to inherit from Actor!
+
+class Artifact():
+    """Help"""
+
+    def __init__(self):
+        super().__init__()
